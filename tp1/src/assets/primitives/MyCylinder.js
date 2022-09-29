ca278ac474a13f1ab929dfb5fed107dd0d3b7461
fernandorego <fernandoluisrego@gmail.com>
2022-09-29 15:58:14 +0100

    Fix MyCylinder texCoords

--- tp1/src/assets/primitives/MyCylinder.js
+++ tp1/src/assets/primitives/MyCylinder.js
@@ -28,13 +28,14 @@ export class MyCylinder extends CGFobject {
 
         let stack_height = this.height / this.stacks;
         let angle = (2 * Math.PI) / this.slices;
-        let current_angle = 0;
         let delta = (this.top - this.base) / this.stacks;
 
         let z_normal = (this.base - this.top) / this.height;
+        let edges = this.slices + 1;
 
         for (let current_stack = 0; current_stack <= this.stacks; current_stack++) {
             let j = 0;
+            let current_angle = 0;
             for (; ;) {
                 this.vertices.push(Math.cos(current_angle) * (this.base + current_stack * delta),
                     Math.sin(current_angle) * (this.base + current_stack * delta),
@@ -42,20 +43,15 @@ export class MyCylinder extends CGFobject {
 
                 this.normals.push(...normalizeVector([Math.cos(current_angle), Math.sin(current_angle), z_normal]));
 
-                this.texCoords.push(j / this.slices, current_stack / this.stacks);
+                this.texCoords.push(j / this.slices, 1 - current_stack / this.stacks);
 
-                if (current_stack < this.stacks && j < this.slices - 1) {
-                    this.indices.push(current_stack * this.slices + j, current_stack * this.slices + j + 1, (current_stack + 1) * this.slices + j);
-                    this.indices.push((current_stack + 1) * this.slices + j, current_stack * this.slices + j + 1, (current_stack + 1) * this.slices + j + 1);
+                if (current_stack < this.stacks && j < this.slices) {
+                    this.indices.push(current_stack * edges + j, current_stack * edges + j + 1, (current_stack + 1) * edges + j);
+                    this.indices.push((current_stack + 1) * edges + j, current_stack * edges + j + 1, (current_stack + 1) * edges + j + 1);
                 }
 
                 current_angle += angle;
-                if (j + 1 === this.slices) { break; } else { j++; }
-            }
-
-            if (current_stack < this.stacks) {
-                this.indices.push(current_stack * this.slices + j, current_stack * this.slices, (current_stack + 1) * this.slices + j);
-                this.indices.push((current_stack + 1) * this.slices + j, current_stack * this.slices, (current_stack + 1) * this.slices);
+                if (j === this.slices) { break; } else { j++; }
             }
         }
 
