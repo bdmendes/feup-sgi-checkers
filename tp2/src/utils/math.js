f5ef6947e2d8b916a161d126f947ce57b16360c3
fernandorego <fernandoluisrego@gmail.com>
2022-11-02 16:14:54 +0000

    Add miliseconds to seconds utils function

--- tp2/src/utils/math.js
+++ tp2/src/utils/math.js
@@ -51,3 +51,7 @@ export function degreesToRadians(degrees) {
 export function vectorDifference([x1, y1, z1], [x2, y2, z2]) {
     return [x1 - x2, y1 - y2, z1 - z2];
 }
+
+export function milisToSeconds(milis) {
+    return (milis / 1000);
+}
