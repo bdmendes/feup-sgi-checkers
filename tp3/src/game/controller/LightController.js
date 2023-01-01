87ae7c8bb602deb53c79f169fdc699714361c0a6
Bruno Mendes <bd_mendes@outlook.com>
2023-01-01 02:14:12 +0000

    Improve light hook

--- tp3/src/game/controller/LightController.js
+++ tp3/src/game/controller/LightController.js
@@ -6,24 +6,39 @@ export class LightController {
         this.scene = scene;
         this.initialLightPosition = null;
         this.disableUpdate = null;
+        this.lightIndex = {}; // per graph
     }
 
     hookSpotlight() {
+        const getIndexFromKey = (key) => {
+            let i = 0;
+            for (const k in this.scene.graph.lights) {
+                if (k === key) return i;
+                i++;
+            }
+            return -1;
+        };
+
         this.disableUpdate = true;
-        this.initialLightPosition = this.scene.lights[this._getIndexFromKey(GAME_SPOTLIGHT_ID)].position;
-        this.scene.lights[this._getIndexFromKey(GAME_SPOTLIGHT_ID)].disable();
+
+        this.lightIndex[this.scene.graph.filename] = getIndexFromKey(GAME_SPOTLIGHT_ID);
+        this.initialLightPosition = this.scene.lights[this.lightIndex[this.scene.graph.filename]].position;
+
+        this.scene.lights[this.lightIndex[this.scene.graph.filename]].disable();
     }
 
     enableSpotlight(piece) {
         this.disableUpdate = true;
+
         this.scene.graph.enabledLights[GAME_SPOTLIGHT_ID] = true;
         this.scene.graph.lights[GAME_SPOTLIGHT_ID][0] = true;
-        this.scene.lights[this._getIndexFromKey(GAME_SPOTLIGHT_ID)].position = [
+        this.scene.lights[this.lightIndex[this.scene.graph.filename]].position = [
             this.initialLightPosition[0] + piece.position[1] * BOARD_POSITION_SIZE,
             this.initialLightPosition[1],
             this.initialLightPosition[2] + piece.position[0] * BOARD_POSITION_SIZE,
             1];
-        this.scene.lights[this._getIndexFromKey(GAME_SPOTLIGHT_ID)].enable();
+        this.scene.lights[this.lightIndex[this.scene.graph.filename]].enable();
+
         this.disableUpdate = false;
     }
 
@@ -31,7 +46,7 @@ export class LightController {
         this.disableUpdate = true;
         this.scene.graph.enabledLights[GAME_SPOTLIGHT_ID] = false;
         this.scene.graph.lights[GAME_SPOTLIGHT_ID][0] = false;
-        this.scene.lights[this._getIndexFromKey(GAME_SPOTLIGHT_ID)].disable();
+        this.scene.lights[this.lightIndex[this.scene.graph.filename]].disable();
     }
 
     updateSpotlight(position) {
@@ -39,19 +54,10 @@ export class LightController {
             return;
         }
 
-        this.scene.lights[this._getIndexFromKey(GAME_SPOTLIGHT_ID)].position = [
+        this.scene.lights[this.lightIndex[this.scene.graph.filename]].position = [
             this.initialLightPosition[0] + position[1] * BOARD_POSITION_SIZE,
             this.initialLightPosition[1],
             this.initialLightPosition[2] + position[0] * BOARD_POSITION_SIZE,
             1];
-    }
-
-    _getIndexFromKey(key) {
-        let i = 0;
-        for (const k in this.scene.graph.lights) {
-            if (k === key) return i;
-            i++;
-        }
-        return -1;
     }
 }
